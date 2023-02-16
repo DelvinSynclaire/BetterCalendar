307d6161b51ff550dbb044ffd18d7458b08cefb0
DelvinSynclaire <dijay.synclaire@gmail.com>
2023-02-15 22:09:15 -0500

    Initial Commit

---
+++ BetterCalendar1.1/BetterCalendar1_1App.swift
@@ -0,0 +1,17 @@
+//
+//  BetterCalendar1_1App.swift
+//  BetterCalendar1.1
+//
+//  Created by Delvin Cockroft on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterCalendar1_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterCalendar1.1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BetterCalendar1.1
+//
+//  Created by Delvin Cockroft on 2/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
